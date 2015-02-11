91965c337144be55d48f86988c7aeefa8e9f4fea
Laura <ladylaurel18@gmail.com>
2015-02-10 19:10:21 -0500

    test

--- js/Preloader.js
+++ js/Preloader.js
@@ -26,8 +26,8 @@ BasicGame.Preloader.prototype = {
 		//	As this is just a Project Template I've not provided these assets, swap them for your own.
 		this.load.image('titlepage', 'images/title.jpg');
 		this.load.atlas('playButton', 'images/play_button.png', 'images/play_button.json');
-		this.load.audio('titleMusic', ['audio/main_menu.mp3']);
-		this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
+		this.load.audio('titleMusic', ['audio/Jupiter.ogg']);
+		//this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
 		//	+ lots of other required assets here
 
 	},
